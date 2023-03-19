f9cafbca45bf1d52c58b9b70e4527ba0db123256
Quan Le Trong <quanltofficial@gmail.com>
2023-03-19 11:35:07 +0700

    update config

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -2,7 +2,8 @@ const redisConfig = {
     redis: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
-        password: process.env.REDIS_PASSWORD
+        password: process.env.REDIS_PASSWORD,
+        db: "4"
     }
 };
 
